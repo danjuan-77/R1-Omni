4fe3338cb893f1f1ce11dcfd370759fa8704eef9
danjuan-77 <youngertutu@gmail.com>
2025-05-01 19:38:56 +0800

    add eval

--- inference.sh
+++ inference.sh
@@ -5,3 +5,16 @@ python inference.py --modal video_audio \
   --model_path /home/tuwenming/Models/StarJiaxing/R1-Omni-0.5B \
   --video_path ./data/test.mp4 \
   --instruct "As an emotional recognition expert; throughout the video, which emotion conveyed by the characters is the most obvious to you?  Output the thinking process in <think> </think> and final emotion in <answer> </answer> tags."
+
+# video only
+python inference.py --modal video \
+  --model_path /home/tuwenming/Models/StarJiaxing/R1-Omni-0.5B \
+  --video_path ./data/test.mp4 \
+  --instruct "As an emotional recognition expert; throughout the video, which emotion conveyed by the characters is the most obvious to you?  Output the thinking process in <think> </think> and final emotion in <answer> </answer> tags."
+
+
+# # audio only
+# python inference.py --modal audio \
+#   --model_path /home/tuwenming/Models/StarJiaxing/R1-Omni-0.5B \
+#   --video_path ./data/test.mp4 \
+#   --instruct "As an emotional recognition expert; throughout the video, which emotion conveyed by the characters is the most obvious to you?  Output the thinking process in <think> </think> and final emotion in <answer> </answer> tags."
